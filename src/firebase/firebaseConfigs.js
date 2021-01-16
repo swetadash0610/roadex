0be89ec0c9e73a49d832dcc53bb594ed1b3f1204
swetadash0610 <swetadash0610@gmail.com>
2021-01-16 20:07:23 +0530

    file upload sorted

--- src/firebase/firebaseConfigs.js
+++ src/firebase/firebaseConfigs.js
@@ -6,12 +6,14 @@ import 'firebase/storage'
 
 // Initialize Firebase
 let config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyDWAOHIH1SZhV_Jpb24DuJKSWvIV5pFloU",
+  authDomain: "roadex-4d93e.firebaseapp.com",
+  databaseURL: "https://roadex-4d93e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "roadex-4d93e",
+  storageBucket: "roadex-4d93e.appspot.com",
+  messagingSenderId: "1072621412763",
+  appId: "1:1072621412763:web:1d43b9e96c4e789068413e",
+  measurementId: "G-LG98PCB2EP"
 }
 
 firebase.initializeApp(config)
