b59e639a7cedff2c9346d83da6a71a245b4514fe
swetadash0610 <swetadash0610@gmail.com>
2021-01-16 08:20:01 +0530

    some changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Home.css';
 import Navbar from "../Includes/Navbar/Navbar"
 import Footer from "../Includes/Footer/Footer"
+import EnhancedTable from "../Admin/Admin"
 
 const Home= () =>{
   return (
       <div>
         <Navbar />
+        <EnhancedTable />
+
         <Footer />
       </div>
   );
